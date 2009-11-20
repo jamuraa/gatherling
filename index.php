c7aa11497f03824a715e59551409a5db1e66eb7d
Michael Janssen <jamuraa@odo.ds9>
2009-11-19 22:15:38 -0600

    added login form to front page

--- index.php
+++ index.php
@@ -10,15 +10,48 @@ print_header("PDCMagic.com | Gatherling | Home");
 your decks in order to see what you played last tourney, last month, or even 
 last year.  You can keep track of all of your decks which are played in PDCMagic.com
 tournaments here, and your ratings for Pauper Magic will also be calculated.</p>
-<p>Some good starting points:
+<p>
+<div class="alpha grid_5"> 
+<b>Some good starting points:</b>
 <ul> 
-<li> <a href="player.php?mode=alldecks"> Enter your own decklists </a> </li> 
 <li> <a href="eventreport.php"> See a list of recent events </a> </li> 
 <li> <a href="decksearch.php"> Search for decks with a certain card </a> </li> 
-<li> <a href="profile.php"> Check out your profile </a> </li>
 <ul> 
-</p>
+</div>
+<div class="grid_5 omega">
+<? $player = Player::getSessionPlayer(); ?>
+<? if ($player != NULL): ?>
+<center> <b> Welcome back <?php echo $player->name ?> </b></center>
+<ul> 
+<li> <a href="profile.php"> Check out your profile </a> </li>
+<li> <a href="player.php?mode=alldecks"> Enter your own decklists </a> </li> 
+</ul>
+<? else: ?> 
+<center> <b> Login to Gatherling </b> </center>
+<form action="login.php" method="post">
+  <table align="center" style="border-width: 0px" cellpadding="3">
+    <tr>
+      <td><b>MTGO Username</b></td>
+      <td><input type="text" name="username" value="" /></td>
+    </tr>
+    <tr>
+      <td><b>Gatherling Password</b></td>
+      <td><input type="password" name="password" value="" /></td>
+    </tr>
+    <tr> 
+      <td colspan="2">
+        <center>
+        <input type="submit" name="mode" value="Log In" /> <br />
+        <a href="register.php">Need to register?</a>
+        </center>
+      </td>
+    </tr>
+  </table>
+</form> 
+<? endif; ?>
+</div> <!-- grid_5 omega (login/links) -->
 
+<div class="clear"></div>
 </div> <!-- gatherlingmain box -->
 
 <div class="box" id="gatherling_news">
