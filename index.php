6b13e37e05c0090a917bfe9f109a37e4dcb75f11
Dabil <dbillett1@gmail.com>
2012-02-22 21:12:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,83 +1,82 @@
-<?php include 'lib.php';
+<?php 
+include 'lib.php'; 
+include 'config.php';
 session_start();
 print_header("$SiteName | Gatherling | Home");
 ?>
-<div class="grid_10 prefix_1 suffix_1"> 
-<div id="gatherling_main" class="box"> 
-<div class="alpha omega grid_10 uppertitle">Gatherling</div> 
-<div class="clear"></div>
-<p>
-Welcome to Gatherling!  With Gatherling you can keep track of 
-your decks in order to see what you played last tournament, last month, or even 
-last year.  You can keep track of all of your decks played here at the <?php echo $SiteName ?> tournaments, and Gatherling will keep a record of how they do.
-</p>
-<p>
-<div class="alpha grid_5"> 
-<b>Some good starting points:</b>
-<ul> 
-<li> <a href="eventreport.php"> See a list of recent events </a> </li> 
-<li> <a href="decksearch.php"> Search for decks with a certain card </a> </li> 
-</ul> 
-<p>
-<b>Gatherling Statistics for <?php echo $SiteName ?>:</b> 
-<ul> 
-<li> There are <?php echo Deck::uniqueCount() ?> unique decks. </li>
-<li> We have recorded <?php echo Match::count() ?> matches from <?php echo Event::count() ?> events.</li> 
-<li> There are <?php echo Player::activeCount() ?> active players in gatherling. (<?php echo Player::verifiedCount() ?> verified) </li>
-</ul>
-</p>
-</div>
-<div class="grid_5 omega">
-<?php $player = Player::getSessionPlayer(); ?>
-<?php if ($player != NULL): ?>
-<b> Welcome back <?php echo $player->name ?> </b>
-<ul> 
-<li> <a href="profile.php">Check out your profile</a> </li>
-<li> <a href="player.php?mode=alldecks">Enter your own decklists</a> </li> 
-<?php $event = Event::findMostRecentByHost($player->name);
-if (!is_null($event)) { ?>
-<li> <a href="event.php?name=<?php echo $event->name ?>">Manage <?php echo $event->name ?></a> </li>
-<?php } ?> 
-<?php if ($player->isHost()) { ?>
-<li> <a href="event.php">Host Control Panel</a> </li>
-<?php } ?> 
-</ul>
-<?php else: ?> 
-<center> <b> Login to Gatherling </b> </center>
-<form action="login.php" method="post">
-  <table class="form" align="center" style="border-width: 0px" cellpadding="3">
-    <tr>
-      <th>MTGO Username</th>
-      <td><input type="text" name="username" value="" /></td>
-    </tr>
-    <tr>
-      <th>Gatherling Password</td>
-      <td><input type="password" name="password" value="" /></td>
-    </tr>
-    <tr> 
-      <td colspan="2" class="buttons">
-        <input type="submit" name="mode" value="Log In" /> <br />
-        <a href="register.php">Need to register?</a>
-      </td>
-    </tr>
-  </table>
-</form> 
-<?php endif; ?>
-</div> <!-- grid_5 omega (login/links) -->
+<div class="grid_10 prefix_1 suffix_1">
+    <div id="gatherling_main" class="box">
+        <div class="alpha omega grid_10 uppertitle">Gatherling</div>
+        <div class="clear"></div>
+        <p>
+            Welcome to Gatherling!  With Gatherling you can keep track of your decks in order to see what you played last tournament, last month, or even last year.  You can keep track of all of your decks played here at the PauperKrew.com tournaments, and Gatherling will keep a record of how they do.
+        </p>
+        <p>
+        <div class="alpha grid_5">
+            <b>Some good starting points:</b>
+            <ul>
+                <li> <a href="eventreport.php"> See a list of recent events </a> </li>
+                <li> <a href="decksearch.php"> Search for decks with a certain card </a> </li>
+            </ul>
+            <p>
+                <b>Gatherling Statistics for PauperKrew.com:</b>
+            <ul>
+                <li> There are <?php echo Deck::uniqueCount() ?> unique decks. </li>
+                <li> We have recorded <?php echo Match::count() ?> matches from <?php echo Event::count() ?> events.</li>
+                <li> There are <?php echo Player::activeCount() ?> active players in gatherling. (<?php echo Player::verifiedCount() ?> verified) </li>
+            </ul>
+        </p>
+        </div>
+        <div class="grid_5 omega">
+                        <?php $player = Player::getSessionPlayer(); ?>
+                        <?php if ($player != NULL): ?>
+            <b> Welcome back <?php echo $player->name ?> </b>
+            <ul>
+                <li> <a href="profile.php">Check out your profile</a> </li>
+                <li> <a href="player.php?mode=alldecks">Enter your own decklists</a> </li>
+                    <?php $event = Event::findMostRecentByHost($player->name);
+                    if (!is_null($event)) { ?>
+                <li> <a href="event.php?name=<?php echo $event->name ?>">Manage <?php echo $event->name ?></a> </li>
+                        <?php } ?>
+                        <?php if ($player->isHost()) { ?>
+                <li> <a href="event.php">Host Control Panel</a> </li>
+                            <?php } ?>
+            </ul>
+                            <?php else: ?>
+            <center> <b> Login to Gatherling </b> </center>
+            <form action="login.php" method="post">
+                <table class="form" align="center" style="border-width: 0px" cellpadding="3">
+                    <tr>
+                        <th>MTGO Username</th>
+                        <td><input type="text" name="username" value="" /></td>
+                    </tr>
+                    <tr>
+                        <th>Gatherling Password</td>
+                        <td><input type="password" name="password" value="" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="buttons">
+                            <input type="submit" name="mode" value="Log In" /> <br />
+                            <a href="register.php">Need to register?</a>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+    <?php endif; ?>
+        </div> <!-- grid_5 omega (login/links) -->
+        <div class="clear"></div>
+    </div> <!-- gatherlingmain box -->
 
-<div class="clear"></div>
-</div> <!-- gatherlingmain box -->
+    <div id="gatherling_news" class="box">
+        <div class="uppertitle alpha omega grid_10"> Gatherling News: Date of News </div>
+        <div class="clear"></div>
+        <ul>
+             <li> This is where you would put any important Gatherling news like updates or bug fixes, etc. </li>
+        </ul>    
+        <div class="clear"></div>
+    </div> <!-- box gatherlingnews -->
 
-<div id="gatherling_news" class="box">
-<div class="uppertitle alpha omega grid_10"> Gatherling News: Date of News </div>
-<div class="clear"></div>
-    <ul>
-     <li> This is where you would put any important Gatherling news like updates or bug fixes, etc. </li>
-    </ul>    
-<div class="clear"></div>
-</div> <!-- box gatherlingnews -->
-
-</div> <!-- grid 10 pre 1 suff 1-->
+    </div> <!-- grid 10 pre 1 suff 1-->
 <?php print_footer(); ?> 
 </div>  <!-- container -->
 </body>
