7e787dbc2a3410980574b55c59744c2c97ce9c57
Michael Janssen <jamuraa@base0.net>
2012-08-21 01:56:30 -0500

    Add chandra style

---
+++ outofservice.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <title>Gatherling | Out of Service</title>
+  </head> 
+  <body>
+    <h1>Gatherling is <em>TEMPORARILY</em> out of service for maintenance.</h1>
+  </body>
+</html>
