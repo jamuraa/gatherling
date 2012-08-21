a08c372d7dc3555a1b58e8378500144b7c673533
Michael Janssen <jamuraa@base0.net>
2012-08-21 01:59:10 -0500

    Add news

---
+++ news.php
@@ -0,0 +1,22 @@
+ <p>
+  <b> 2012-05-31 </b> -
+  It's an update!  This time it is mostly bugfixes, but maybe there will be some time in the future to
+  make some more improvements.  We've gotten a bunch of help from the community (thanks Dabil) to make
+  some cleanup happen under the hood.
+  <ul>
+    <li> Players with no tournament entries can now login and work fine </li>
+    <li> Lots of under-the-hood stuff made better </li>
+  </ul>
+ </p>
+ <p>
+  <b> 2011-10-10 </b> -
+  If you thought that the last time between updates was long, I have news for you.  At any rate, I've been given some time to get some stuff done again and I have been able to fix some bugs and provide some new functionality for everyone.
+  <ul>
+    <li> You can now pre-register for events if the event host allows it.  More features to be added soon related to this. </li>
+    <li> Deck search is re-enabled now, so you can find popular decks and decks with specific cards. </li>
+    <li> Event hosts will find some fancy UI improvements in the back end. </li>
+    <li> You can't make a deck without a name anymore. </li>
+    <li> A bunch of backend bugfixes and improvements. </li>
+   </ul>
+</p>
+<br />
