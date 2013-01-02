8dba47f561dc69143543ef6652ba88d4ef422ff0
Dabil <dbillett1@gmail.com>
2013-01-02 15:10:04 -0500

    Update models/Series.php
    Moves Series Season Standings to Model so it can be easily accessed from multiple places.

--- models/Series.php
+++ models/Series.php
@@ -596,5 +596,59 @@ class Series {
     }
     echo "</select>";
   }
+  
+  private static function reverse_total_sort($a, $b) { 
+      if ($a['.total'] == $b['.total']) {
+          return 0;
+      }
+      return ($a['.total'] < $b['.total']) ? 1 : -1; 
+  }   
+  
+  public static function seasonStandings($series, $season) {
+      $seasonevents = $series->getSeasonEventNames($season);
+      $points = $series->seasonPointsTable($season);
+      $cutoff = $series->getSeasonCutoff($season);
+      uasort($points, 'Series::reverse_total_sort');
+    
+      echo "<h3><center>Scoreboard for {$series->name} season {$season}</center></h3>";
+      echo "<table class=\"scoreboard\">";
+      echo "<tr class=\"top\"><th>Place</th><th>Player</th><th>Total</th>";
+    
+      foreach ($seasonevents as $evname) {
+          $shortname = preg_replace("/^{$series->name} /", '', $evname);
+          $reportlink = "eventreport.php?event=" . urlencode($evname);
+          echo "<th><a href=\"{$reportlink}\">{$shortname}</a></th>";
+      }
+      echo "</tr>";
+
+      $count = 0;    
+      foreach ($points as $playername => $pointar) {
+          $player = new Player($playername);
+          $count++;
+          $classes = "";  
+          if ($count % 2 != 0) { 
+              $classes = "odd";
+          }
+          if ($count == $cutoff) { 
+              $classes .= " cutoff";
+          } 
+          echo "<tr class=\"{$classes}\"> ";
+          echo "<td>{$count}</td><td class=\"playername\">{$player->linkTo()}</td><td>{$pointar['.total']}</td> "; 
+
+          foreach ($seasonevents as $evname) { 
+              if (isset($pointar[$evname])) { 
+                  if (is_array($pointar[$evname])) { 
+                      echo "<td><span title=\"{$pointar[$evname]['why']}\">{$pointar[$evname]['points']}</span></td>";
+                  } else { 
+                      echo "<td>{$pointar[$evname]}</td>"; 
+                  } 
+              } else { 
+                  echo "<td></td> "; 
+              } 
+          }  
+          echo "</tr>"; 
+      }
+      echo "</table>"; 
+  }    
 }
 
